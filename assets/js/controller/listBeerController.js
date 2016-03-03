3ddd2d9d9b7a96dcae8ab1b23a1237b5a6bb06af
Murilo de Siqueira Ferreira <Murilo.Ferreira@deal.br>
2016-03-03 11:59:11 -0300

    test orderBy

--- assets/js/controller/listBeerController.js
+++ assets/js/controller/listBeerController.js
@@ -1,3 +1,11 @@
 angular.module('beer').controller('listBeerController', function ($scope, beers) {
 	$scope.beers = beers.data;
+
+	// OrderBy
+	$scope.predicate = 'alcohol';
+	$scope.reverse = false;	
+	$scope.order = function(predicate) {
+		$scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : true;
+		$scope.predicate = predicate;
+	};
 })
